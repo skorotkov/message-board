a68bda05e85183e5bb9f3a6ff15cb6f24621c596
Sergey Korotkov <serge.korotkov@gmail.com>
2017-08-24 02:24:22 +0700

    working github auth

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,9 +2,4 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  model() {
-    if (this.get('session').isAuthenticated) {
-      return this.get('store').findRecord('github-user', '#');
-    }
-  }
 });
---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    if (this.get('session').isAuthenticated) {
+      return this.get('store').findRecord('github-user', '#');
+    }
+  }
+});
